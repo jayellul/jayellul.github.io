5248d162cfe6b293f64fdeb33ad3bd902d63fca8
jasonellul <jason.j.ellul@gmail.com>
2019-08-14 16:34:06 -0400

    trying to fix safari

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
 import '../css/Homepage.css';
 
-import Dimensions from 'react-dimensions'
-
 class Homepage extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { width: 0, height: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+
   render() {
-    const half = this.props.containerHeight / 2;
-    console.log(this.props);
+    const half = this.state.height / 2;
+    console.log(this.state);
     return (
       <div className='homepage-wrapper'>
-        <div style={{ background: '#000', position: 'fixed', left: 0, bottom: 0, width: '100vw', height: half }} />
-        <div style={{ height: half, display: 'flex' }}>
-          <h1 className='title-name'>JASON ELLUL</h1>
-        </div>
-        <div style={{ height: half, display: 'flex' }}>
-          <div className='text-container'>
-            <p className='homepage-text'>User interface designer.</p>
-            <p className='homepage-text'>Full stack mobile &amp; web developer.</p>
-            <p className='homepage-text'>SoundCloud rapper.</p>
+        <div style={{ background: '#000', position: 'fixed', left: 0, bottom: 0, width: '100vw', height: '50vh' }} />
+        <div style={{ height: '100vh', position: 'relative' }}>
+          <div style={{ height: '50%', display: 'flex', }}>
+            <h1 className='title-name'>JASON ELLUL</h1>
+          </div>
+          <div style={{ height: '50%', display: 'flex' }}>
+            <div className='text-container'>
+              <p className='homepage-text'>User interface designer.</p>
+              <p className='homepage-text'>Full stack mobile &amp; web developer.</p>
+              <p className='homepage-text'>SoundCloud rapper.</p>
+            </div>
           </div>
         </div>
       </div >
@@ -27,4 +46,4 @@ class Homepage extends Component {
 }
 
 
-export default Dimensions()(Homepage);
+export default Homepage;
